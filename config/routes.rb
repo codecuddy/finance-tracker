5705fe607f63481a71d56a0aff2f1073b92dcef1
Charlie Cuddy <charliecuddy4@gmail.com>
2016-05-03 05:07:54 +0000

    Add friendship and friend search functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => { :registrations => "user/registrations" }
   resources :user_stocks, except: [:show, :edit, :update]
+  resources :user, only: [:show]
+  resources :friendships
   # get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -12,6 +14,8 @@ Rails.application.routes.draw do
   get 'my_portfolio', to: "users#my_portfolio"
   get 'search_stocks', to: "stocks#search"
   get 'my_friends', to: "users#my_friends"
+  get 'search_friends', to: "users#search"
+  post 'add_friend', to: "users#add_friend"
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
